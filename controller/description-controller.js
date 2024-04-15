7dd17e3a13fe331af2bc4171dec7162c390adb45
Nlim97 <nobellimbu1@gmail.com>
2024-04-15 15:38:23 +0100

    /api responds with an object with desciptions, queries and format of the relative path

--- controller/description-controller.js
+++ controller/description-controller.js
@@ -3,7 +3,6 @@ const { fetchDescription } = require("../model/description-model")
 function getDescription(request, response, next){
     fetchDescription().then((data) => {
         const parsedData = JSON.parse([data])
-        console.log({parsedData})
         response.status(200).send({parsedData})
     })
 }
